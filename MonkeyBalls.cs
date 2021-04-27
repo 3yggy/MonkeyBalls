01f1d0130b266a00e85371fa08922fe39c2e03a4
Ziggy <thegreengreenbean@gmail.com>
2021-04-27 00:11:59 -0400

    balls

--- MonkeyBalls.cs
+++ MonkeyBalls.cs
@@ -12,16 +12,16 @@ namespace MonkeyBalls
         /// <param name="owner">The object containing the field containing the monkey ball.</param>
         /// <param name="fieldName">The name of the field containing the monkey ball.</param>
         /// <param name="val">to set the monkey ball to.</param>
-        public static void SetMonkeyPoop(object owner, string fieldName, object val) {
+        public static void SetMonkeyBalls(object owner, string fieldName, object val) {
             FieldInfo field = owner.GetType().GetField(fieldName,BindingFlags.Public|BindingFlags.NonPublic|BindingFlags.Instance);
-            field.SetValue(owner, SetMonkeyPoop(field.GetValue(owner), val));
+            field.SetValue(owner, SetMonkeyBalls(field.GetValue(owner), val));
         }
 
         /// <summary>Sets the value of the object contained in the generated monkey ball.</summary>
         /// <param name="host">The object the monkey ball is embedded in.</param>
         /// <param name="val">to set the monkey ball to.</param>
         /// <returns>The updated monkey ball.</returns>
-        public static T SetMonkeyPoop<T>(T host, object val) {
+        public static T SetMonkeyBalls<T>(T host, object val) {
             if(!(host is ImAMonkeyBall)) {
                 AttachMonkeyBall(ref host);
             }
@@ -33,7 +33,7 @@ namespace MonkeyBalls
         /// <summary>Gets the value of the object contained in the generated monkey ball.</summary>
         /// <param name="host">The object the monkey ball is embedded in.</param>
         /// <returns>the value of the object contained in the generated monkey ball.</returns>
-        public static T GetMonkeyPoop<T>(object host) {
+        public static T GetMonkeyBalls<T>(object host) {
             Type hostType = host.GetType();
             if(!(host is ImAMonkeyBall)) {
                 return default(T);
